6d2239df8030c4539423c29644d290d19ead813d
Eugene-Fed <eugene.a.fed@gmail.com>
2023-03-30 11:56:42 +0600

    bug fixed

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import re
 
 TEMP_KEYWORDS_PATH = r"D:\Downloads\requests.csv"  # будет заменено на поиск реального расположения папки
 # TEMP_KEYWORDS_PATH = r"D:\Downloads\wb-template.csv"      # файл для выгрузки в кнопку бабло (функция не работает)
-KEYWORD_COUNT_LIMIT = 3
+KEYWORD_COUNT_LIMIT = 10
 REQUIRED_PLACE_INDEXES = (1, 3, 5)                   # Задаем позиции по которым будем собирать статистику (начиная с 1)
 
 
@@ -30,7 +30,7 @@ def log_in(window_id: str, account: dict, login_data: dict):
     bm.click_key(window_id, login_data['pass_key'], login_data['pass_value'], key='enter', sleep=1)       # Press ENTER
 
 
-def parse_stat_table(field_tag_name: str, field_tag_value: str, sleep=3, positions=REQUIRED_PLACE_INDEXES) -> list:
+def parse_stat_table(field_tag_name: str, field_tag_value: str, sleep=0, positions=REQUIRED_PLACE_INDEXES) -> list:
     """
     Gets bids for required positions
     :param field_tag_name: element_type for searching statistics table
@@ -56,8 +56,14 @@ def get_key_stat(window_id: str, field_tag_name, field_tag_value, keyword, sleep
     bm.set_text(window_id, field_tag_name, field_tag_value, keyword, sleep=sleep)       # input key at text field
     bm.click_key(window_id, field_tag_name, field_tag_value, key='enter', sleep=1)      # press ENTER on keyboard
     print(f'Keyword: "{keyword}"')
-    bids = parse_stat_table('class', 'MuiDataGrid-row', sleep)              # get Bids for `REQUIRED_PLACE_INDEXES`
-    categories = bm.find_elements(element_type='class', name='sc-frJOEA')[0].text.split('\n')   # get `Prior categories`
+
+    categories = []
+    bids = []
+    time.sleep(sleep)
+    cat_div = bm.find_elements(element_type='class', name='sc-frJOEA')  # get `Prior categories` element
+    if cat_div:
+        categories = cat_div[0].text.split('\n')            # if `Prior categories` element not empty get categories
+        bids = parse_stat_table('class', 'MuiDataGrid-row')  # get Bids for `REQUIRED_PLACE_INDEXES`
     return {'bids': bids, 'categories': categories}
 
 
@@ -81,9 +87,19 @@ if __name__ == '__main__':
         i = 0
         reader = csv.reader(f, delimiter=',')
         # print(help(reader))
+
+        with open('stat.txt', 'a') as f:  # log file
+            # t = time.localtime()
+            # current_time = time.strftime("%H:%M:%S", time.localtime())
+            f.write(f'\n{time.strftime("%H:%M:%S", time.localtime())}\n#########################')
+
         for raw in reader:
             if i < KEYWORD_COUNT_LIMIT:
-                print(get_key_stat(window_id, 'name', 'value', raw[0], sleep=6))
+                stat = get_key_stat(window_id, 'name', 'value', raw[0], sleep=6)
+                with open('stat.txt', 'a', encoding='utf-8') as f:
+                    keyword, frequency = raw[0], f'{int(raw[1]):,}'.replace(',', ' ')
+                    f.write(f'\n"{keyword}" - {frequency}\n{stat}')
+                print(stat)
             else:
                 break
             i += 1
