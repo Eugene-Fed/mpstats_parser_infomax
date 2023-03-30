b0f2b8881a709a243eb30b51b623653c20776668
Eugene-Fed <eugene.a.fed@gmail.com>
2023-03-30 10:02:21 +0600

    Getting a priority category is ready

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ import re
 TEMP_KEYWORDS_PATH = r"D:\Downloads\requests.csv"  # будет заменено на поиск реального расположения папки
 # TEMP_KEYWORDS_PATH = r"D:\Downloads\wb-template.csv"      # файл для выгрузки в кнопку бабло (функция не работает)
 KEYWORD_COUNT_LIMIT = 3
-SEARCHING_INDEXES = (1, 3, 5)                   # Задаем позиции по которым будем собирать статистику (начиная с 1)
+REQUIRED_PLACE_INDEXES = (1, 3, 5)                   # Задаем позиции по которым будем собирать статистику (начиная с 1)
 #
 
 
 def set_text(window_id: str, field_tag_name: str, field_tag_value: str, text: str, sleep=1):
     """
+    TODO - функция не имеет смысла. Переписать скрипт на использование вызова `bm.set_text()`
     Interface to use Parser functions
     :param window_id:
     :param field_tag_name: Type of tag to search
@@ -28,10 +29,11 @@ def set_text(window_id: str, field_tag_name: str, field_tag_value: str, text: st
     :param sleep:
     :return: None
     """
-    bm.input_text(handler=window_id, element=field_tag_name, name=field_tag_value, data=text, sleep=sleep)
+    bm.set_text(handler=window_id, element=field_tag_name, name=field_tag_value, data=text, sleep=sleep)
 
 
 def press_key(window_id: str, field_tag_name: str, field_tag_value: str, key='enter', sleep=1):
+    # TODO Функция бесполезна. Использовать вызов ниже напрямую в скрипте
     bm.click_key(handler=window_id, element=field_tag_name, name=field_tag_value, key=key, sleep=sleep)
 
 
@@ -48,7 +50,7 @@ def log_in(window_id: str, account: dict, login_data: dict):
     press_key(window_id, login_data['pass_key'], login_data['pass_value'])   # Press ENTER for send login form
 
 
-def parse_stat_table(field_tag_name, field_tag_value, sleep=3, positions=SEARCHING_INDEXES) -> list:
+def parse_stat_table(field_tag_name: str, field_tag_value: str, sleep=3, positions=REQUIRED_PLACE_INDEXES) -> list:
     """
     Gets bids for required positions
     :param field_tag_name: element_type for searching statistics table
@@ -62,7 +64,7 @@ def parse_stat_table(field_tag_name, field_tag_value, sleep=3, positions=SEARCHI
     rows = bm.find_elements(element_type=field_tag_name, name=field_tag_value)      # gets all rows grom stat table
     for p in positions:                 # searching only required positions
         cells = rows[p-1].find_elements(By.TAG_NAME, 'div')         # lists start from `0` and that's why `p-1`
-        nums = re.findall(r'\d+', cells[7].text)                    # from value we gets only a number
+        nums = re.findall(r'\d+', cells[7].text)                    # we get only a number from value
         # nums = re.findall(r'\d*\.\d+|\d+', s)                     # for float
         cpm = [int(n) for n in nums][0]                             # we have only one number
         print(f'Позиция {p}, Ставка: {cpm}')
@@ -73,8 +75,9 @@ def parse_stat_table(field_tag_name, field_tag_value, sleep=3, positions=SEARCHI
 def get_key_stat(window_id: str, field_tag_name, field_tag_value, keyword, sleep=3):
     set_text(window_id, field_tag_name=field_tag_name, field_tag_value=field_tag_value, text=keyword, sleep=sleep)
     press_key(window_id, field_tag_name=field_tag_name, field_tag_value=field_tag_value, sleep=sleep)
-    print(keyword)
-    return parse_stat_table('class', 'MuiDataGrid-row', sleep)
+    bids = parse_stat_table('class', 'MuiDataGrid-row', sleep)              # get Bids for `REQUIRED_PLACE_INDEXES`
+    categories = bm.find_elements(element_type='class', name='sc-frJOEA')[0].text.split('\n')   # get `Prior categories`
+    return {'bids': bids, 'categories': categories}
 
 
 if __name__ == '__main__':
